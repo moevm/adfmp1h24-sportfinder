61382f14c30dbc3be5971b53d232780c2e1926b8
Evgeny Shkvirya <zhshkvir@yandex-team.ru>
2024-03-05 00:16:32 +0300

    #3: App init

---
+++ SportFinder/app/build.gradle.kts
@@ -0,0 +1,114 @@
+plugins {
+    alias(libs.plugins.com.android.application)
+    alias(libs.plugins.org.jetbrains.kotlin.android)
+    id("kotlin-kapt")
+    id("dagger.hilt.android.plugin")
+}
+
+android {
+    namespace = "ru.moevm.sportfinder"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "ru.moevm.sportfinder"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        vectorDrawables {
+            useSupportLibrary = true
+        }
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.4.2"
+    }
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
+}
+
+dependencies {
+
+    implementation(libs.core.ktx)
+    implementation(libs.lifecycle.runtime.ktx)
+    implementation(libs.activity.compose)
+    implementation(platform(libs.compose.bom))
+    implementation(libs.ui)
+    implementation(libs.ui.graphics)
+    implementation(libs.ui.tooling.preview)
+    implementation(libs.material)
+    implementation(libs.material3)
+
+    // Google Maps Location Services
+    implementation(libs.googlemaps.compose)
+    implementation(libs.googlemaps.playservices)
+    implementation(libs.googlemaps.utils)
+
+    // Retrofit
+    implementation(libs.retrofit)
+    implementation(libs.retrofit.coroutines.adapter)
+    implementation(libs.gson)
+    implementation(libs.gson.converter)
+
+    // Dagger2
+    implementation(libs.dagger2)
+    implementation(libs.dagger2.android)
+    kapt(libs.dagger2.android.support)
+    kapt(libs.dagger2.android.processor)
+    kapt(libs.dagger2.compiler)
+
+    // Coroutines
+    implementation(libs.coroutines.android)
+    implementation(libs.coroutines.core)
+
+    // Hilt
+    implementation(libs.hilt.android)
+    kapt(libs.hilt.android.compiler)
+    kapt(libs.hilt.compiler)
+    implementation(libs.hilt.navigation.compose)
+
+
+    // ConstraintLayout
+    implementation(libs.androidx.constraintlayout.compose)
+
+    // Room
+    implementation(libs.room.runtime)
+    kapt(libs.room.compiler)
+
+    // Kotlin Extensions and Coroutines support for Room
+    implementation(libs.room.ktx)
+
+    implementation(libs.datastore.preferences)
+
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.test.ext.junit)
+    androidTestImplementation(libs.espresso.core)
+    androidTestImplementation(platform(libs.compose.bom))
+    androidTestImplementation(libs.ui.test.junit4)
+    debugImplementation(libs.ui.tooling)
+    debugImplementation(libs.ui.test.manifest)
+}
